be9695434e33fd051d30f23a8e18f6fc9acb06ba
smasiu <szymon.masko32@gmail.com>
2020-09-19 19:59:13 +0200

    Updates

---
+++ component---src-pages-404-tsx-0ff9c51d52073828b4e4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),u=t("B7F5"),c=t("z2p8");n.default=function(){return l.a.createElement(u.a,null,l.a.createElement(c.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-0ff9c51d52073828b4e4.js.map
