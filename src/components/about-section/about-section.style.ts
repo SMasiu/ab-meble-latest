d157d67b4e64b33af50c0b4780a7a4561b5e028c
smasiu <szymon.masko32@gmail.com>
2020-09-20 15:23:23 +0200

    about section rwd

--- src/components/about-section/about-section.style.ts
+++ src/components/about-section/about-section.style.ts
@@ -1,32 +1,71 @@
 import styled from 'styled-components'
-import { getPageWidth, getHeadingUnderline } from 'styles/css.helpers'
+import { getPageWidth, getHeadingUnderline, getHeadingCenterUnderline } from 'styles/css.helpers'
 
 export const AboutSectionWrapper = styled.section`
   ${getPageWidth}
   display: grid;
   grid-template-columns: 1fr 1fr;
+  grid-template-areas: 'image article';
+  gap: 50px;
   height: 80vh;
   min-height: 700px;
+
+  @media (max-width: 1350px) {
+    padding: 0 50px;
+  }
+
+  @media (max-width: 950px) {
+    grid-template-columns: 1fr;
+    grid-template-areas: 'article' 'image';
+    height: auto;
+    min-height: 0;
+    margin-bottom: 50px;
+    justify-items: center;
+  }
+
+  @media (max-width: 650px) {
+    width: 90%;
+    padding: 0;
+  }
 `
 
 export const AboutImageSection = styled.section`
   align-self: center;
+  grid-area: image;
 `
 
 export const ImageWrapper = styled.figure`
-  width: 600px;
+  max-width: 600px;
+
+  @media (max-width: 950px) {
+    max-width: 400px;
+  }
 `
 
 export const ArticleImage = styled.img``
 
 export const AboutArtice = styled.article`
+  grid-area: article;
   margin-top: 25%;
+
+  @media (max-width: 1350px) {
+    margin-top: 40%;
+  }
+
+  @media (max-width: 950px) {
+    margin-top: 0;
+  }
 `
 
 export const AboutHeader = styled.header`
   display: flex;
   flex-direction: column-reverse;
   ${getHeadingUnderline}
+
+  @media (max-width: 950px) {
+    text-align: center;
+    ${getHeadingCenterUnderline}
+  }
 `
 
 export const AboutContentWrapper = styled.section`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Layout } from 'components/layout/layout'
 import { SEO } from 'components/seo/seo'
 // import { HeroSection } from 'components/hero-section/hero-section'
-// import { AboutSection } from 'components/about-section/about-section'
+import { AboutSection } from 'components/about-section/about-section'
 import { ProductsSection } from 'components/products-section/products-section'
 import { ProjectsSecton } from 'components/projects-section/projects-section'
 
@@ -10,8 +10,8 @@ const IndexPage: React.FC = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      {/* <HeroSection />
-      <AboutSection /> */}
+      {/* <HeroSection /> */}
+      <AboutSection />
       <ProductsSection />
       <ProjectsSecton />
     </Layout>
