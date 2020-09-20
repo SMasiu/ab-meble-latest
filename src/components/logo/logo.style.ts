4b4a4ddd5932dcbdaeb441f0fb505fd73818f8ad
smasiu <szymon.masko32@gmail.com>
2020-09-20 14:00:43 +0200

    logo rwd

--- src/components/logo/logo.style.ts
+++ src/components/logo/logo.style.ts
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 export const LogoWrapper = styled.figure`
   width: 150px;
   padding: 10px 0;
+  transition: width 0.3s;
+  will-change: width;
+
+  @media (max-width: 1000px) {
+    width: 125px;
+  }
 `
 
 export const LogoImg = styled.img``
