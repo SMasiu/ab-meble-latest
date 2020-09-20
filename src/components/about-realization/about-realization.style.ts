417dffaa1658de2b511e375ea23cd0be68d68124
smasiu <szymon.masko32@gmail.com>
2020-09-20 18:18:46 +0200

    about realization rwd

--- src/components/about-realization/about-realization.style.ts
+++ src/components/about-realization/about-realization.style.ts
@@ -4,20 +4,33 @@ import { getPageWidth, getHeadingUnderline, getBgColor } from 'styles/css.helper
 export const AboutRealizationWrapper = styled.section`
   ${getPageWidth}
   margin: 50px auto 100px auto;
+
+  @media (max-width: 1350px) {
+    width: 90%;
+  }
 `
 
 export const RealizationHeader = styled.header`
   ${getHeadingUnderline}
+  margin-bottom: 40px;
 `
 
 export const RealizationContentWrapper = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 50px;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 export const RealizationArticle = styled.article`
   margin-top: 100px;
+
+  @media (max-width: 1024px) {
+    margin-top: 25px;
+  }
 `
 
 export const RealizationContent = styled.p`
@@ -32,7 +45,11 @@ export const ReferenceWrapper = styled.section`
 
 export const ReferenceImageWrapper = styled.figure`
   padding: 50px;
-  max-width: 500px;
+  max-width: 600px;
+
+  @media (max-width: 1024px) {
+    max-width: 750px;
+  }
 `
 
 export const ReferenceImg = styled.img``
