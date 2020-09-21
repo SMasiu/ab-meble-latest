e4618512f66420e5420afcb2743607e913360d64
smasiu <szymon.masko32@gmail.com>
2020-09-21 19:06:53 +0200

    projects rwd

--- src/components/project-item/project-item.style.ts
+++ src/components/project-item/project-item.style.ts
@@ -31,17 +31,49 @@ export const ProjectItemWrapper = styled.section`
     transform: translateX(-50%);
     ${getBgColor('light300')};
   }
+
+  @media (max-width: 1024px) {
+    &::before {
+      content: none;
+    }
+  }
 `
 
 export const ProjectItemContent = styled.section`
   position: relative;
   display: grid;
   grid-template-columns: 250px 6fr 4fr;
+  grid-template-areas: 'minimap image article';
   gap: 25px;
+
+  @media (max-width: 1350px) {
+    grid-template-columns: 250px 1fr;
+    grid-template-areas: 'minimap image' 'article article';
+  }
+
+  @media (max-width: 1024px) {
+    grid-template-columns: 1fr;
+    grid-template-areas: 'image' 'minimap' 'article';
+  }
+
+  @media (max-width: 700px) {
+    gap: 0;
+  }
 `
 
 export const ProjectItemMinimap = styled.aside`
+  grid-area: minimap;
   ${getHeadingCenterUnderline}
+
+  @media (max-width: 1024px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 25px;
+  }
+
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 export const MinimapImage = styled.figure<UrlProps>`
@@ -55,9 +87,23 @@ export const MinimapImage = styled.figure<UrlProps>`
   ${({ url }: UrlProps) => css`
     background-image: url(${url});
   `}
+
+  @media (max-width: 1024px) {
+    width: 100%;
+    margin: 0;
+  }
+
+  @media (max-width: 700px) {
+    height: 400px;
+  }
+
+  @media (max-width: 550px) {
+    height: 300px;
+  }
 `
 
 export const ProjectItemImage = styled.figure<UrlProps>`
+  grid-area: image;
   width: 100%;
   height: 450px;
   background-size: cover;
@@ -65,14 +111,28 @@ export const ProjectItemImage = styled.figure<UrlProps>`
   ${({ url }: UrlProps) => css`
     background-image: url(${url});
   `}
+
+  @media (max-width: 700px) {
+    display: none;
+  }
 `
 
 export const ProjectItemArticle = styled.article`
+  grid-area: article;
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding-left: 20px;
+
+  @media (max-width: 1350px) {
+    margin: 25px auto 0;
+    max-width: 600px;
+  }
+
+  @media (max-width: 1024px) {
+    margin: 0 auto;
+  }
 `
 
 export const ArticleHeader = styled.header``
@@ -81,6 +141,11 @@ export const ItemHeader = styled.header`
   position: relative;
   margin-left: 275px;
   margin-bottom: 15px;
+
+  @media (max-width: 1024px) {
+    text-align: center;
+    margin-left: 0;
+  }
 `
 
 export const ArticleHeading = styled(HeadingH3)`
--- src/components/projects-page/projects-page.style.ts
+++ src/components/projects-page/projects-page.style.ts
@@ -4,12 +4,20 @@ import { getPageWidth, getHeadingCenterUnderline } from 'styles/css.helpers'
 export const ProjectsPageWrapper = styled.section`
   padding: 75px 0;
   ${getPageWidth};
+
+  @media (max-width: 1350px) {
+    width: 90%;
+  }
 `
 
 export const ProjectPageHeader = styled.header`
   ${getHeadingCenterUnderline}
   text-align: center;
   margin-bottom: 125px;
+
+  @media (max-width: 1024px) {
+    margin-bottom: 75px;
+  }
 `
 
 export const ProjectsList = styled.section`
