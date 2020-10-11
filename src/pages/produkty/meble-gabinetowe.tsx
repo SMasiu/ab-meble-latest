c7ab8f7243838db7e320434dbce799c3866ab419
smasiu <szymon.masko32@gmail.com>
2020-10-11 13:18:08 +0200

    product images

--- src/pages/produkty/meble-gabinetowe.tsx
+++ src/pages/produkty/meble-gabinetowe.tsx
@@ -3,13 +3,13 @@ import { Layout } from 'components/layout/layout'
 import { SEO } from 'components/seo/seo'
 import { ProductItem } from 'components/product-item/product-item'
 import { OtherCategories } from 'components/other-categories-section/others-categories-section'
-import { productInput } from 'inputs/product-page.input'
+import { productOfficeFurnitureInput } from 'inputs/product-page.input'
 
 const ProductCategoryPage: React.FC = () => {
   return (
     <Layout>
       <SEO title="Meble gabinetowe" />
-      <ProductItem product={productInput} />
+      <ProductItem product={productOfficeFurnitureInput} />
       <OtherCategories />
     </Layout>
   )
